c708255c0a6c03d11ae8794ee3e89c0198a97c3b
Eulalie Idjaton <idjaeulalie@gmail.com>
2024-08-07 17:43:26 +0100

    mise à jour du fichier main et scraper

--- main.py
+++ main.py
@@ -1,52 +1,24 @@
 from fastapi import FastAPI, HTTPException
-from scraper import CoursDeviseScraper
-from scraper1 import CurrencyConverterScraper
+from scraper import CurrencyConverterScraper
 
 app = FastAPI()
 
 # Initialisation de l'instance de la classe avec l'URL du site
-scraper = CoursDeviseScraper(url='https://www.bceao.int/fr/cours/cours-des-devises-contre-Franc-CFA-appliquer-aux-transferts')
-scraper1 = CurrencyConverterScraper()
+scraper = CurrencyConverterScraper()
 
 # Accueil
 @app.get('/')
 async def bienvenu():
-    return 'Soyez les bienvenus sur API des Cours des Devises!'
-
-# API du scraping
-@app.get('/api/scrape')
-async def scrape_data(Devise: str, Date: str):
-    try:
-        scraper.scrape_data(devise=Devise, date=Date)
-        scraper.save_data('data.json')
-        return {'message': 'Scraping terminé avec succès', 'data': scraper.data[Date]}
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-# API du scraping puis le retour de la requête
-@app.get('/api/coursdevises')
-async def get_cours_devise(Devise: str, Date: str):
-    try:
-        scraper.scrape_data(devise=Devise, date=Date)
-        return {
-            "Devise": f"{Devise}/XOF",
-            "Echange": scraper.data[Date]
-        }
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return f"Bienvenus sur l'API de conversions de devises!"
 
 # API du scraping de conversion des devises
 @app.get('/api/convertion_devise')
-async def convert(Montant: int, Devise_montant: str, Devise_voulue: str):
-    url = f'https://www.xe.com/fr/currencyconverter/convert/?Amount={Montant}&From={Devise_montant}&To={Devise_voulue}'
+async def convert(Amont: int, Currency_to_convert: str, Expected_Currency: str):
+    url = f'https://www.xe.com/fr/currencyconverter/convert/?Amount={Amont}&From={Currency_to_convert}&To={Expected_Currency}'
     try:
-        result = scraper1.get_donnee_converties(url)
+        result = scraper.get_donnee_converties(url)
         montant_sortis = result[0]['Montant_sortis']
-        return f"Resultat en {Devise_voulue}: {montant_sortis}"
+        return f"Resultat en {Expected_Currency}: {montant_sortis}"
     
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
--- scraper.py
+++ scraper.py
@@ -1,60 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import json
-import time
+import requests
+from bs4 import BeautifulSoup
 
-class CoursDeviseScraper:
-    def __init__(self, url: str):
-        self.url = url
-        self.driver = webdriver.Chrome()
-        self.data = {}
-
-    def close(self):
-        self.driver.quit()
-
-    def is_weekend_or_holiday(self, date: str):
-        holidays = ["2024-01-01", "2024-03-31", "2024-04-01", "2024-05-01", "2024-06-01", "2024-12-25"]
-        return time.strptime(date, '%Y-%m-%d').tm_wday >= 5 or date in holidays
-
-    def scrape_data(self, devise: str, date: str):
-        self.driver.get(self.url)
-        wait = WebDriverWait(self.driver, 10)
-
-        year, month, day = map(int, date.split('-'))
-
-        if self.is_weekend_or_holiday(date):
-            raise ValueError("La date sélectionnée est un week-end ou un jour férié.")
-
-        rectangular_frame = wait.until(EC.element_to_be_clickable((By.ID, 'date_picker')))
-        rectangular_frame.click()
-
-        tds = self.driver.find_elements(By.CSS_SELECTOR, 'table.ui-datepicker-calendar td')
-        day_td = [td for td in tds if td.text == str(day) and td.is_displayed()]
+class CurrencyConverterScraper:
+    def __init__(self):
+        pass
+    
+    def get_soup(self,url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        return soup
+    
+    def get_donnee_converties(self, url):
+        soup = self.get_soup(url)
+        elements = soup.find_all('main', class_='sc-d5cf72bb-3 lmBDhC')
+        data = []
         
-        if not day_td:
-            raise ValueError(f"Le jour sélectionné n'est pas disponible pour le scraping.")
-
-        try:
-            self.driver.execute_script("arguments[0].click();", day_td[0])
-
-            table = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="box_cours"]/table')))
-            rows = table.find_elements(By.TAG_NAME, 'tr')
-
-            for row in rows:
-                cells = row.find_elements(By.TAG_NAME, 'td')
-                if len(cells) == 3 and cells[0].text == devise:
-                    self.data[date] = {
-                        #'Devise': cells[0].text,
-                        'Achat': cells[1].text,
-                        'Vente': cells[2].text
-                    }
-                    break
-
-        except Exception as e:
-            raise ValueError(f"Une erreur s'est produite lors du scraping: {e}")
-
-    def save_data(self, file_path: str):
-        with open(file_path, 'w', encoding='utf-8') as f:
-            json.dump(self.data, f, ensure_ascii=False, indent=4)
+        for elt in elements:
+            montant_entres = elt.find('div', class_='sc-73a056d4-0 sc-b8d6d0bc-0 eVknbT eoNUEg').text
+            devise = elt.find('div', id='midmarketToCurrency').text
+            montant_sortis = elt.find('p', class_='sc-e08d6cef-1 fwpLse').text
+            data.append({
+                'Montant_entres': montant_entres,
+                'XOF': devise,
+                'Montant_sortis': montant_sortis
+            })
+        
+        return data
+    
+    
